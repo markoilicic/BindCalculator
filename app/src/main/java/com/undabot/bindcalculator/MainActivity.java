254fda26ec81a010e7c090d393149d6cf5c66e25
Marko Ilicic <soso.the.man@gmail.com>
2014-12-20 13:33:32 +0100

    Finished MainActivity & imported Butterknife

--- app/src/main/java/com/undabot/bindcalculator/MainActivity.java
+++ app/src/main/java/com/undabot/bindcalculator/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.widget.Button;
 import android.widget.TextView;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import rx.Observable;
 import rx.android.observables.ViewObservable;
 import rx.functions.Action1;
@@ -12,29 +14,28 @@ import rx.functions.Action1;
 
 public class MainActivity extends ActionBarActivity {
 
+    @InjectView(R.id.btn_click)
+    Button btnClick;
+    @InjectView(R.id.tv_result)
+    TextView tvResultLabel;
     private int mNumOne = 0;
     private int mNumTwo = 1;
     private int rez;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnClick = (Button) findViewById(R.id.btn_click);
-        final TextView tvResultLabel = (TextView) findViewById(R.id.tv_result);
+        ButterKnife.inject(this);
 
-        final Observable<Button> btnClickObservable = ViewObservable.clicks(btnClick, false);
+        Observable<Button> btnClickObservable = ViewObservable.clicks(btnClick, false);
         btnClickObservable.subscribe(new Action1<Button>() {
             @Override
             public void call(Button button) {
-
                 tvResultLabel.setText(String.valueOf(getResult()));
             }
         });
-
-
     }
 
     public int getResult() {
